f24d1b3974b3c29d621a43cc43f9141e84faf147
NewSouthWave <tigers0110@naver.com>
2024-07-04 20:07:55 +0900

    Initial Commit

---
+++ MoviePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by Nam on 2024/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
